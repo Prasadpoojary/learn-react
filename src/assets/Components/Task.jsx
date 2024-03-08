9465369e77beabe886c589838227872613440420
Prasad Poojary <prasadspoojar@gmail.com>
2024-03-09 00:00:50 +0530

    map list - item list dynamic - challenge

--- src/assets/Components/Task.jsx
+++ src/assets/Components/Task.jsx
@@ -2,18 +2,34 @@ import React, { useState } from "react";
 
 const Task=()=>
 {
-   
-    const [input, setInput]=useState("");
+    // early challenge - may have mistakes 
+    const [input,setInput]=useState("");
+    const [items,addItem]=useState(["Chilly","Kabab","Fish","Egg"]);
 
     const handleChange=(event)=>
     {
         setInput(event.target.value);
     }
 
+    const handleClick=()=>
+    {
+        if(input!="")
+        addItem([...items,input])
+        
+        
+    }
+
    //  React.Fragment
     return <>
-        <input type="text" onChange={handleChange} />
-        <h1>{input}</h1>
+        <h1>Items List</h1>
+        <input type="text" onChange={handleChange}/>
+        <button onClick={handleClick}>Add</button>
+
+        <ul>
+            {
+                items.map(item=><li key={item}>{item}</li>)
+            }
+        </ul>
         </>
 }
 
