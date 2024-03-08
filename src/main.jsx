d2dac2f874bde061f6dbcda57226805fc114b70a
Prasad Poojary <prasadspoojar@gmail.com>
2024-03-09 00:02:40 +0530

    typo - moved component to src

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Task from './assets/Components/Task'
+import Task from './Components/Task'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
