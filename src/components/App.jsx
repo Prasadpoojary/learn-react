846b5ab608386b7dbf2701344cfe3ac3750a3a1f
Prasad Poojary <prasadspoojar@gmail.com>
2024-03-16 23:41:06 +0530

    fragment : added React.Fragment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,31 @@
+import React from "react"
 
-
-export const App=()=>
+const App=()=>
 {
-    return <h1>Hello Roopa</h1>
+    const name="Anu"
+
+    // // JSX must have only one parent, so we can use either of below
+    // return <React.Fragment>
+    //         <h1>Hello {name}</h1>
+    //         <p>I am a good girl</p>
+    // </React.Fragment>
+
+        return <>
+            <h1>Hello {name}</h1>
+            <p>I am a good girl</p>
+    </>
+            
+  
 }
+
+export default App
+
+
+/*
+JSX : Javascript + HTML 
+Babel compiler converts JSX into plain HTML in browser at runtime
+
+*/
 
 
 
