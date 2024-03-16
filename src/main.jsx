5af14078482279f5bcb43493144b7c9bff7db2db
Prasad Poojary <prasadspoojar@gmail.com>
2024-03-16 23:28:28 +0530

    component : first component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Task from './Components/Task'
+import { App } from './components/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Task />
+    <App />
   </React.StrictMode>,
 )
