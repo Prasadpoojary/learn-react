c8b2e3194956cfd8084aee01feca3ab66b93dfa6
Prasad Poojary <prasadspoojar@gmail.com>
2024-03-24 20:24:34 +0530

    useEffect : hook basics

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './sectionStyle.css'
 const Section = () => {
 
@@ -6,20 +6,54 @@ const Section = () => {
   // useState(default_value) which returns a list [current_value, setValue_function]
     
     const [input,setInput]=useState()
-    
+
+  
     const [todos,setTodos]=useState([])
 
     
     const handleClick=()=>
     {
+        if( !input || input.trim().length==0)
+        {
+          alert("Please enter valid input");
+          return 
+        }
         setTodos((prev)=>{return [...prev,input]})
-
-        // prev =["Prasad","Anu"]
-        // input = Aadya
-        // [...prev,input] =["Prasad","Anu","Aadya"]
-       // console.log(todos);
+        console.log(todos)
     }
- 
+
+
+
+
+  //useEffect(arrow_function,[depedency1,dependecy2,....]);
+
+  //// 1. without dependency array useEffect will be called on every event on component
+  // useEffect(()=>{
+  //   console.log("useEffect function executed...");
+  // });
+
+
+  //// 2. with 'empty' dependency array useEffect will be called only once when component rendered
+  //   useEffect(()=>{
+  //   console.log("useEffect function executed...");
+  // },[]);
+
+
+  //// 3. with dependency array useEffect will be called whenever dependency items updated
+  //   useEffect(()=>{
+  //   console.log("useEffect function executed...");
+  // },[todos,input]);
+
+  //   useEffect(()=>{
+  //   console.log(todos);
+  // },[todos]);
+
+
+  
+  //   useEffect(()=>{
+  //    document.title=todos.length+" Todes added";
+  // },[todos]);
+
 
   return (
     <section>
